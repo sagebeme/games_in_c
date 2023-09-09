7fb950570a2fcf69a5ede3ca6c5645807888ff21
sagebeme <eudicas5k@gmail.com>
2023-09-09 17:41:36 +0300

    initializing SDL window,creating and making a renderer

--- game/src/main.c
+++ game/src/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include "./constants.h"
 
-void initialize_window(void){
-	printf("wewe game ibambe \n");
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+int initialize_window(void){
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		fprintf(stderr, "ERROR INTITIALIZING SDL. \n");
+		return FALSE;
+	}
+	window = SDL_CreateWindow(
+			"Brick Game by Sage", 
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			WINDOW_WIDTH,/*width*/
+			WINDOW_HEIGHT, /*height*/
+			SDL_WINDOW_BORDERLESS
+			);
+
+	if (!window) {
+		fprintf(stderr, "Error creating window. \n");
+		return FALSE;
+	}
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer");
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
 int main () {
