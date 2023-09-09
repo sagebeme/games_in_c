12b4338a7bd2a779e881a7766e2d86d9a78b40f9
sagebeme <eudicas5k@gmail.com>
2023-09-09 16:59:56 +0300

    testing the function

---
+++ game/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+void initialize_window(void){
+	printf("wewe game ibambe \n");
+}
+
+int main () {
+	printf("Game is running...\n");
+	initialize_window();	
+	return 0;
+}
